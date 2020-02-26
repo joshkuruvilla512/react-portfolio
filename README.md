2d85838be448f3a4c5ad5c790f0293822126b371
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2020-02-25 19:09:13 -0600

    Trying

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# react-portfolio
+# Unit 20 React Homework: React Portfolio
+
+A simple Portfolio
+
+## Preview
+<img width="100%" alt="Screen Shot 2020-02-25 at 6 18 20 PM" src="https://user-images.githubusercontent.com/57737037/75299685-50aacc80-57fb-11ea-997b-5cefb5966cef.png">
+
+
+## Special Thanks
+
+1. Crystal Ly for the inspiration
+2. Rahul Bhatia for the inspiration
+
+
---
+++ src/resumeData.js
@@ -0,0 +1,121 @@
+let resumeData = {
+  "imagebaseurl": "https://joshkuruvilla512.github.io/",
+  "name": "Joshua Kuruvilla",
+  "role": "Full Stack Developer",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": "",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/joshua-kuruvilla/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/joshkuruvilla512",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "http://twitter.com/JoshuaKuruvilla",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "I am a Full Stack Developer with background experience in the Automotive Industry",
+  "address": "Austin, TX",
+  "website": "https://react-portfolio-ut.herokuapp.com/",
+  "education": [
+    {
+      "UniversityName": "University of Texas at Austin",
+      "specialization": "Full Stack Development",
+      "MonthOfPassing": "March",
+      "YearOfPassing": "2020",
+      "Achievements": "Certificate"
+    },
+    {
+      "UniversityName": "Oakland University",
+      "specialization": "Marketing",
+      "MonthOfPassing": "April",
+      "YearOfPassing": "2017",
+      "Achievements": "Bachelors"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Dürr Systems",
+      "specialization": "Logistics Manager",
+      "MonthOfLeaving": "July",
+      "YearOfLeaving": "2018-November 2019",
+      "Achievements": "Worked to make sure that the parts and equipment are delivered to the installation team on time and in the condition required for proper installation"
+      /* <li>
+        <p>Works to make sure that the parts and equipment are delivered to
+      the installation team on time and in the condition required for proper installation</p>
+      </li>
+      <li>
+        <p>Participated in Pre Installation Planning/Scheduling meetings to develop the logistics plan</p>
+      </li>
+      <li>
+        <p>Used SAP for tracking/identifying parts on arrival at site and properly label parts so they can be located in the installation plan
+          </p>
+      </li>
+      <li>
+    <p>Trained People in Durr’s Logistics Management procedures</p>
+      </li>
+    </ul> */
+    },
+    {
+      "CompanyName": "Nissan Automotive of North America",
+      "specialization": "Purchasing Analyst",
+      "MonthOfLeaving": "May",
+      "YearOfLeaving": "2017-June 2018",
+      "Achievements": "Analyze cost reduction data and related processes while making recommendations for improvement when possible"
+    }
+  ],
+  "skillsDescription": "",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
